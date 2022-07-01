49538bb6da9449942a87e96415f3887fb1324174
maxis <maxwellmutuku@gmail.com>
2022-06-30 19:31:01 -0700

    first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** 
+ *
+ * main - Entry pint
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+
+{
+	"Programming is like building a multilingual puzzle\n"
+
+	Return (0);
+}
